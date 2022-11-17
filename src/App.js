7af6faa9b4595795d07cf36c32f6ff93de93b035
Crownkay <55544959+Crownkay@users.noreply.github.com>
2022-11-17 09:22:32 -0800

    question component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Questions from "./components/Questions";
 
 
 function App() {
   return (
     <>
       <Navbar />
+      <Questions />
     </>
   );
 }
---
+++ src/components/Questions.js
@@ -0,0 +1,12 @@
+import Reat from "react";
+
+function Questions() {
+    return (
+        <section>
+            <p>What is the full meaning of HTML</p>
+
+        </section>
+    )
+}
+
+export default Questions
